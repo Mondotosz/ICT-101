f2a6e1afe299cbe8c94550bd0d3aeb4927bd48d7
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-02-19 11:53:17 +0100

    first js file create doesn't really work though

---
+++ html/js-playground/first-try.js
@@ -0,0 +1,8 @@
+
+
+let canvas=document.getElementById("main-canvas");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#009688";
+// ctx.strokeStyle = "#009688";
+// ctx.ellipse(0,0,100,100,0,0,0);
+ctx.fillRect(50,50,100 ,100);
